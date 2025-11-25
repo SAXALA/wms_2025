d899bb81337f69aab61ede83260c0be34943bf10
Hline <3329163446@qq.com>
2025-11-25 08:25:37 +0000

    init wms

---
+++ src/main/java/com/example/wms_2025/Wms2025Application.java
@@ -0,0 +1,13 @@
+package com.example.wms_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wms2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wms2025Application.class, args);
+	}
+
+}
